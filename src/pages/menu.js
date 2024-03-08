8fec5d50c4489ac2f0f0374d108505aaffa34317
xswitch <bivarmong@gmail.com>
2024-03-08 11:23:29 +0100

    Creates menu from an object where it places each section in the container with the same starting name (wineSection) would mean the name of the section in the object would have to be wine

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -58,7 +58,95 @@ function createMenu() {
         })
     }
 
+    function newMenuItem(title, price, description = false, bottlePrice = false) {
+        return {
+            title,
+            price,
+            description,
+            bottlePrice,
+        }
+    }
 
+    const menuItems = {
+        entrees: [
+            newMenuItem('Beef Tenderloin', 26, 'Roasted with a spicy chili infusion, served medium rare and topped with chimichurri salsa'),
+            newMenuItem('Delmonico', 28, 'Hand-cut 20 oz bone-in sirloin steak topped with pink peppercorn, cognac and leek reduction'),
+            newMenuItem('Prime Rib', 28, 'Aged 8 oz prime rib slow roasted in rock salt and served with au jus and fresh grated horseradish'),
+            newMenuItem('New Orleans Bouillabaisse', 16, 'Petite prawns, Andouille sausage, mirepoix and Cajun cream sauce served over red rice'),
+            newMenuItem('Oregon Trout', 19, 'Sautéed fresh boneless trout with tarragon butter sauce, parsley and caramelized shallots'),
+            newMenuItem('Squash Ravioli', 10, 'Ambercup squash, shallot brown butter and green apple chutney'),
+            newMenuItem('Coho Salmon', 23, 'Over-roasted line-caught salmon with chervil and kumquat pesto'),
+            newMenuItem('Veal Scallopini', 29, 'Thinly-pounded grilled veal chop stuffed with yellow peppers. San Marzano tomatoes and ricotta salad'),
+            newMenuItem('Duck', 26, 'Oven-roasted organic duck boneless breast topped with Champagne vinaigrette, pancetta and pickled red onions'),
+            newMenuItem("Grandma's Roast Beef", 27, 'Our farmhouse special'),
+        ],
+    
+        
+        sides: [
+            newMenuItem('Petite Feta Quiche', 9),
+            newMenuItem('Mashed Potatoes', 6),
+            newMenuItem('Gnocchi & Cheese', 11),
+            newMenuItem('Steamed Vegetables', 3),
+            newMenuItem('Sautéed Mushrooms', 3),
+            newMenuItem('Creamed Spinach', 3),
+        ],
+    
+        dessert: [
+            newMenuItem('House-Made Ice Cream of the Month', 9),
+            newMenuItem('House-Made Sorbet of the Month', 7),
+            newMenuItem('Walnut Sponge Cake with Fresh Cream', 11),
+        ],
+    
+        wine: [
+            newMenuItem('Colterenzio Pinot Grigio', 8, 'Italy 2018', 28),
+            newMenuItem('Mudhouse Sauvignon Blanc', 8, 'New Zealand 2018', 29),
+            newMenuItem('Graham Beck Chenin Blanc', 7, 'South Africa 2009', 27),
+            newMenuItem('Charles & Charles Syrah Rose', 6, 'Columbia Valley 2016', 20),
+        ],
+    }
+
+    function createMenuItem(item, parent) {
+
+        const menuItemContainer = new El('div', {
+            classes: 'menuItem',
+            parent,
+        }).element
+
+        const menuItem = {
+            title: new El('h4', {
+                classes: 'menuItemTitle',
+                parent: menuItemContainer,
+                text: item.title
+            }),
+            price: new El('h4', {
+                classes: 'menuItemPrice',
+                parent: menuItemContainer,
+                text: item.price,
+            }),
+            description: new El('p', {
+                classes: 'menuItemDescription',
+                parent: menuItemContainer,
+                text: item.description,
+            }),
+        }
+        if (item.bottlePrice != false) {
+            menuItem.bottlePrice = new El('h4', {
+                classes: 'menuItemBottlePrice',
+                parent: menuItemContainer,
+                text: item.bottlePrice,
+            })
+        }
+    }
+
+    function createAllItems(menuObj) {
+        for (const section in menuObj) {
+            menuObj[section].forEach(item => {
+                createMenuItem(item, containers[section + 'Section'].element)
+            })
+        }
+    }
+
+    createAllItems(menuItems)
 }
 
 export default createMenu;
