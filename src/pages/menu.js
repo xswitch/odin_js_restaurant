c2b3f96fc214e828c2ab88752676eeca2c0acafa
xswitch <bivarmong@gmail.com>
2024-03-08 10:28:47 +0100

    Added the layout for the menu Sectioned off different dishes

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -15,8 +15,50 @@ function createMenu() {
         menuWrapper: new El('div', {
             classes: 'menuWrapper',
             parent: '.menu'
+        }),
+        entreesSection: new El('div', {
+            classes: 'menuSection entreesSection',
+            parent: '.menuWrapper'
+        }),
+        sidesSection: new El('div', {
+            classes: 'menuSection sidesSection',
+            parent: '.menuWrapper'
+        }),
+        dessertSection: new El('div', {
+            classes: 'menuSection dessertSection',
+            parent: '.menuWrapper'
+        }),
+        wineSection: new El('div', {
+            classes: 'menuSection wineSection',
+            parent: '.menuWrapper'
+        })
+
+    }
+
+    const menuTitles = {
+        entreesTitle: new El('h3', {
+            classes: 'menuTitle',
+            parent: containers.entreesSection.element,
+            text: 'MAIN ENTREES',
+        }),
+        sidesTitle: new El('h3', {
+            classes: 'menuTitle',
+            parent: containers.sidesSection.element,
+            text: 'SIDES'
+        }),
+        dessertTitle: new El('h3', {
+            classes: 'menuTitle',
+            parent: containers.dessertSection.element,
+            text: 'DESSERTS',
+        }),
+        wineTitle: new El('h3', {
+            classes: 'menuTitle',
+            parent: containers.wineSection.element,
+            text: 'WINES'
         })
     }
+
+
 }
 
 export default createMenu;
